7c59e4e58a4786fc5f016cb79d9d973ee279cab2
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-11-08 10:46:17 +0200

    WIP: Numbers pad

--- src/elm/Hi.elm
+++ src/elm/Hi.elm
@@ -2,6 +2,7 @@ module Hi where
 
 import Effects exposing (Effects)
 import Html exposing (..)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
@@ -37,9 +38,11 @@ update action model =
 view : Signal.Address Action -> Model -> Html
 view address model =
   div []
-    [ button [ onClick address (AddDigit 1) ] [ text "1" ]
-    , button [ onClick address (AddDigit 2) ] [ text "2" ]
-    , button [ onClick address (AddDigit 3) ] [ text "3" ]
-    , button [ onClick address (AddDigit 4) ] [ text "4" ]
+    [ div [class "numbers"]
+      [ button [ onClick address (AddDigit 1) ] [ text "1" ]
+      , button [ onClick address (AddDigit 2) ] [ text "2" ]
+      , button [ onClick address (AddDigit 3) ] [ text "3" ]
+      , button [ onClick address (AddDigit 4) ] [ text "4" ]
+    ]
     , div [ ] [ text (toString model) ]
     ]
