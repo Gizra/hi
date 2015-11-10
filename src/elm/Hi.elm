702be96b574be07dfee4033f5db047a95580cd4d
Amitaibu <amitai@gizra.com>
2015-11-10 11:36:23 +0200

    more fixes

--- src/elm/Hi.elm
+++ src/elm/Hi.elm
@@ -4,18 +4,29 @@ import Effects exposing (Effects)
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Http exposing (Error)
 import String exposing (length, repeat)
+import Task exposing (Task, succeed)
 
+import Debug
 
 -- MODEL
 
+type Status =
+  Init
+  | Fetching
+  | Fetched
+  | HttpError Http.Error
+
 type alias Model =
   { pinCode : String
+  , status : Status
   }
 
 initialModel : Model
 initialModel =
   { pinCode = ""
+  , status = Init
   }
 
 init : (Model, Effects Action)
@@ -27,33 +38,51 @@ init =
 
 -- UPDATE
 
-type Action = AddDigit Int
+type Action
+  = AddDigit Int
+  | SubmitCode
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     AddDigit digit ->
-    ( { model |
-          pinCode <-
-            if length model.pinCode < 4
-              then model.pinCode ++ toString(digit)
-              else model.pinCode
-      }
-    , Effects.none
-    )
+      let
+        (pinCode', effects) =
+          if length model.pinCode < 4
+            then
+              ( model.pinCode ++ toString(digit)
+              , Effects.none
+              )
+            else
+              ( model.pinCode
+              , Task.succeed SubmitCode |> Effects.task
+              )
+      in
+        ( { model | pinCode <- pinCode' }
+        , effects
+        )
+
+    SubmitCode ->
+      let
+        d = Debug.log "SubmitCode" True
+      in
+      ( model, Effects.none )
 
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
+  let
+    digitButton : Int -> Html
+    digitButton digit =
+      button [ onClick address (AddDigit digit) ] [ text <| toString digit ]
+  in
   div [class "number-pad"]
-    [ div [class "number-buttons"]
-    -- ( List.map digitButton [1..9] )
-    [ button [ onClick address (AddDigit 1) ] [ text "1" ] ]
+    [ div
+        [ class "number-buttons" ]
+        (List.map digitButton [1..9])
+
+
+
     , div [ ] [ text <| repeat (length model.pinCode) "*" ]
     ]
-
-
-digitButton : Int -> Html
-digitButton digit =
-  button [ ] [ text <| toString digit ]
