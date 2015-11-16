bf1729661ddb4faa9733ec68fbcdc045025b6318
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-11-16 22:26:36 +0200

    Handling responses

--- src/elm/Hi.elm
+++ src/elm/Hi.elm
@@ -3,10 +3,10 @@ module Hi where
 import Config exposing (backendUrl)
 import Effects exposing (Effects, Never)
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode as JD
+import Json.Decode as Json exposing ((:=))
 import Json.Encode as JE
 import String exposing (length)
 import Task
@@ -25,6 +25,11 @@ type Status =
   | Fetching
   | Fetched
   | HttpError Http.Error
+
+type alias Response =
+  { employee : String
+  , action : String
+  }
 
 type alias Model =
   { pincode : String
@@ -51,7 +56,7 @@ init =
 type Action
   = AddDigit Int
   | SubmitCode
-  | ShowResponse (Result Http.Error String)
+  | ShowResponse (Result Http.Error Response)
   | SetMessage Message
 
 update : Action -> Model -> (Model, Effects Action)
@@ -68,7 +73,10 @@ update action model =
             then Task.succeed SubmitCode |> Effects.task
             else Effects.none
       in
-        ( { model | pincode <- pincode' }
+        ( { model
+          | pincode <- pincode'
+          , message <- Empty
+          }
         , effects'
         )
 
@@ -76,7 +84,6 @@ update action model =
       let
         url : String
         url = Config.backendUrl ++ "/api/v1.0/session"
-
       in
         if model.status == Fetching || model.status == Fetched
           then
@@ -92,29 +99,56 @@ update action model =
 
     ShowResponse result ->
       case result of
-        Ok token ->
+        Ok session ->
           ( { model | status <- Fetched }
           , Task.succeed (SetMessage (Success "Success")) |> Effects.task
           )
-        Err msg ->
-          ( { model | status <- HttpError msg }
-          , Task.succeed (SetMessage (Error "something is wrong")) |> Effects.task
-          )
+        Err error ->
+          let
+            message =
+              getErrorMessageFromHttpResponse error
+          in
+            ( { model | status <- HttpError error }
+            , Task.succeed (SetMessage <| Error message) |> Effects.task
+            )
 
     SetMessage message ->
       ( { model | message <- message }
       , Effects.none
       )
+
+getErrorMessageFromHttpResponse : Http.Error -> String
+getErrorMessageFromHttpResponse error =
+  case error of
+    Http.Timeout ->
+      "Connection has timed out"
+
+    Http.BadResponse code _ ->
+      if | code == 401 -> "Wrong username or password"
+         | code == 429 -> "Too many login requests with the wrong username or password. Wait a few hours before trying again"
+         | code >= 500 -> "Some error has occured on the server"
+         | otherwise -> "Unknow error has occured"
+
+    Http.NetworkError ->
+      "A network error has occured"
+
+    Http.UnexpectedPayload message ->
+      "Unexpected response: " ++ message
+
+    _ ->
+      "Unexpected error: " ++ toString error
+
+
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div
     [ class "keypad" ]
-    [ (viewMessage model.message)
-    ,  div
+    [ div
         [ class "number-buttons" ]
         ( List.map (digitButton address) [0..9] |> List.reverse )
+    , (viewMessage model.message)
     ]
 
 viewMessage : Message -> Html
@@ -122,11 +156,11 @@ viewMessage message =
   let
     (className, string) =
       case message of
-        Empty -> ("none", "")
+        Empty -> ("", "")
         Error msg -> ("error", msg)
         Success msg -> ("success", msg)
   in
-    div [ class className ] [ text string ]
+    div [ id "status-message", class className ] [ text string ]
 
 
 digitButton : Signal.Address Action -> Int -> Html
@@ -140,17 +174,14 @@ getJson : String -> String -> Effects Action
 getJson url pincode =
   Http.send Http.defaultSettings
     { verb = "POST"
-    , headers = [ ("access-token", "lXlTh7PR30mQN316SN3LofK95krQjCltBnygfjkleyQ") ]
+    , headers = [ ("access-token", "BgIoWiCd_5WwC4R3tq3tjdBUPUPYZtUYA0AZObkFMBg") ]
     , url = url
     , body = ( Http.string <| dataToJson pincode )
     }
-    |> Http.fromJson decodePincode
+    |> Http.fromJson decodeResponse
     |> Task.toResult
     |> Task.map ShowResponse
     |> Effects.task
-
-
-
 
 dataToJson : String -> String
 dataToJson code =
@@ -158,11 +189,9 @@ dataToJson code =
     <| JE.object
         [ ("pincode", JE.string code) ]
 
-decodeAccessToken : JD.Decoder String
-decodeAccessToken =
-  JD.at ["access_token"] <| JD.string
-
-
-decodePincode : JD.Decoder String
-decodePincode =
-  JD.at ["pincode"] <| JD.string
+decodeResponse : Json.Decoder Response
+decodeResponse =
+  Json.at ["data"]
+    <| Json.object2 Response
+      ("employee" := Json.string)
+      ("action" := Json.string)
