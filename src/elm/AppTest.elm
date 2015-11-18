65b7bb52796e3d172f79c63983ee0a63dfe3034b
Amitaibu <amitai@gizra.com>
2015-11-18 09:59:51 +0200

    Add Send last digit

--- src/elm/AppTest.elm
+++ src/elm/AppTest.elm
@@ -11,11 +11,23 @@ addDigitSuite : Test
 addDigitSuite =
   suite "Add digit Action Suite"
     [ test "Send first digit" (assertEqual "5" (.pincode <| fst(addDigit 5)))
+    , test "Send last digit" (assertEqual "1234" (.pincode <| fst(addDigitOnFullPincode 4)))
     ]
 
 addDigit : Int -> (App.Model, Effects App.Action)
 addDigit val =
   App.update (App.AddDigit val) App.initialModel
+
+addDigitOnFullPincode : Int -> (App.Model, Effects App.Action)
+addDigitOnFullPincode val =
+  let
+    model =
+      App.initialModel
+
+    model' =
+      { model | pincode <- "123" }
+  in
+    App.update (App.AddDigit val) model'
 
 all : Test
 all =
