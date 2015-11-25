d27398f6cbbefe234ba26a4c1bd8234a5651baaf
YaronMiro <yaron@gizra.com>
2015-11-25 13:27:24 +0200

    move line down

--- src/elm/Config.elm
+++ src/elm/Config.elm
@@ -6,7 +6,7 @@ backendUrl = "http://localhost/productivity/www"
 accessToken : String
 accessToken = "iNP3aLho0fYT_mxgsd5ZrYXUcJh6i_Rku2sG6dY4G-k"
 
-project = {
-  name = "Negawatt project"
+project =
+  { name = "Negawatt project"
   , id = 33
   }
